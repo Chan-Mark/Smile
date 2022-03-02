4ce9527e43b2f589f151a622701768bb670d903b
Chan-Mark <18285@wc.school.nz>
2022-03-02 14:34:26 +1300

    Added menu, contact and home page to app

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def render_homepage():
     return '<h1>Hello, World!</h1>'
+    return render_template("home.html")
 
 
-app.run(host='0.0.0.0')
+@app.route('/menu')
+def render_menu_page():
+    return render_template("menu.html")
+
+
+@app.route('/contact')
+def render_contact():
+    return render_template("contact.html")
+
+
+app.run(host="0.0.0.0")
