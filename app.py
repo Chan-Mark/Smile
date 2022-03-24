295be4698053e649991da108ef5e392cf0735d92
Chan-Mark <18285@wc.school.nz>
2022-03-24 14:20:57 +1300

    Changed some css (flask 11 + 12)

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, session, redirect
 import sqlite3
 from sqlite3 import Error
+from flask_bcrypt import Bcrypt
 
 #DB_NAME = "C:/Users/18285/PycharmProjects/smilev2/smile.db"
 DB_NAME = "smile.db"
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 app.secret_key = "diereallybest"
 
 
@@ -47,12 +49,6 @@ def render_contact():
     return render_template("contact.html", logged_in=is_logged_in())
 
 
-# Login video plan:
-# - set your secret key
-# - what is a session?
-# - redirects
-# - review registering, then show how the login process is similar
-
 @app.route('/login', methods=["GET", "POST"])
 def render_login_page():
     if is_logged_in():
@@ -78,16 +74,16 @@ def render_login_page():
             return redirect("/login?error=Email+invalid+or+password+incorrect")
 
         # check if the password is incorrect for that email address
-        if db_password != password:
-            return redirect("/login?error=Email+invalid+or+password+incorrect")
-        # if not bcrypt.check_password_hash(db_password, password):
-        #     return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
-        #
+
+        if not bcrypt.check_password_hash(db_password, password):
+            return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
+
         session['email'] = email
         session['userid'] = userid
         session['firstname'] = firstname
         print(session)
         return redirect('/')
+
     return render_template('login.html', logged_in=is_logged_in())
 
 
@@ -107,6 +103,7 @@ def render_signup_page():
         if len(password) < 8:
             return redirect('/signup?error=Password+must+be+8+characters+or+more')
 
+        hashed_password = bcrypt.generate_password_hash(password)
         con = create_connection(DB_NAME)
 
         query = "INSERT INTO customer(id, fname, lname, email, password) " \
@@ -114,7 +111,7 @@ def render_signup_page():
 
         cur = con.cursor()  # You need this line next
         try:
-            cur.execute(query, (fname, lname, email, password))  # This line actually executes the query
+            cur.execute(query, (fname, lname, email, hashed_password))  # This line actually executes the query
         except sqlite3.IntegrityError:
             return redirect('/signup?error=Email+is+already+used')
 
